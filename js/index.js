49b014853e65c3e65c44af0b65241156863da9b2
Jhontorbul <jhon-bula-@hotmail.com>
2022-06-05 15:37:55 -0500

    Se agrego Libreria para el menu estilo Apple, se agrego los estilos y jquery local

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 //const fullpageEl = document.getElementById('fullpage')
+$( document ).ready(function() {
+
 const menuBtn = document.querySelector('.menu__btn')
 const navigation = document.querySelector('.navigation')
 const navCloseBtn = document.querySelector('.navigation__close__btn')
@@ -23,3 +25,45 @@ blurOverlay.addEventListener(CLICK, toggleNavigation)
 	//autoScrolling: true,
 	//scrollBar: true,
 //})
+$('.slider').slick({
+	dots: false,
+	infinite: false,
+	speed: 500,
+	slidesToShow: 7,
+	slidesToScroll: 4,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: true,
+		  prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+		  nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
+		}
+	  },
+	  {
+		breakpoint: 800,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 2,
+		  dots: true,
+		  prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+		  nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  dots:true,
+		  slidesToShow: 3,
+		  slidesToScroll: 2,
+		  arrows:true,
+		  prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+		  nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>'
+		}
+	  }
+	]
+  });
+});
